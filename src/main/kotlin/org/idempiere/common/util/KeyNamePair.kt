193fc4de2c0ebd7da69caac51cbc92cf435c0045
David Podhola <david@podhola.net>
2018-11-28 21:30:38 +0100

    # Conflicts: #	.gitignore #	src/main/java/org/compiere/model/I_AD_Table.java #	src/main/kotlin/org/idempiere/common/util/KeyNamePair.kt #	src/main/kotlin/org/idempiere/common/util/NamePair.kt

--- src/main/kotlin/org/idempiere/common/util/KeyNamePair.kt
+++ src/main/kotlin/org/idempiere/common/util/KeyNamePair.kt
@@ -1,3 +1,3 @@
 package org.idempiere.common.util
 
-data class KeyNamePair(val key: Int, val _name: String): NamePair(_name)
+data class KeyNamePair(val key: Int, val _name: String) : NamePair(_name)
