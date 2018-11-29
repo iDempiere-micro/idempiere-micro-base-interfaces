80f5e20608405e07463bbf5aa02cb788e9ceb180
David Podhola <david@podhola.net>
2018-11-29 20:20:17 +0100

    "Contains Name" interfaces added

---
+++ src/test/kotlin/org/compiere/model/tests/InheritanceTest.kt
@@ -0,0 +1,6 @@
+package org.compiere.model.tests
+
+import software.hsharp.core.models.INamedEntity
+import software.hsharp.core.models.ISearchableByKey
+
+interface TestPartner : ISearchableByKey, INamedEntity
