8b06e2274d1fd2086baef4e0d3ec436dbeeff5be
davidpodhola <david@podhola.net>
2019-02-15 14:08:19 +0100

    context added

--- src/main/kotlin/software/hsharp/core/models/EnvironmentService.kt
+++ src/main/kotlin/software/hsharp/core/models/EnvironmentService.kt
@@ -1,8 +1,11 @@
 package software.hsharp.core.models
 
+import java.util.Properties
+
 /**
  * Environment where the current execution is happening. Always have the current user, client etc.
  */
 interface EnvironmentService {
     val clientId: Int
+    val context: Properties
 }
