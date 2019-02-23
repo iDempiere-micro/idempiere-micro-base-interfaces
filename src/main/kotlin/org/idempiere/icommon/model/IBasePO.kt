07321068b8f88a50af28259395d6bc238c063df4
davidpodhola <david@podhola.net>
2019-02-23 09:13:17 +0100

    method comments

--- src/main/kotlin/org/idempiere/icommon/model/IBasePO.kt
+++ src/main/kotlin/org/idempiere/icommon/model/IBasePO.kt
@@ -4,9 +4,18 @@ package org.idempiere.icommon.model
  * The ORM properties to identify a persistent object.
  */
 interface IBasePO : software.hsharp.core.models.IBasePO {
+    /**
+     * Primary Key
+     */
     val id: Int
 
+    /**
+     * Organization owning this persistent object.
+     */
     val orgId: Int
 
+    /**
+     * Id of table this persistent object is stored in.
+     */
     val tableId: Int
 }
