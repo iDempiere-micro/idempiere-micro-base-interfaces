4845dada58bf0de730e96328be3c1f759b84f374
davidpodhola <david@podhola.net>
2019-03-12 21:58:13 +0100

    code comments

--- src/main/kotlin/software/hsharp/core/models/BaseDataService.kt
+++ src/main/kotlin/software/hsharp/core/models/BaseDataService.kt
@@ -2,6 +2,13 @@ package software.hsharp.core.models
 
 import org.idempiere.icommon.model.IPO
 
+/**
+ * Base interface to be extended by all the base data services that allow e.g. to get all the rows
+ * for the current client
+ */
 interface BaseDataService<T : IPO> {
+    /**
+     * Get all [T]s for the current client
+     */
     fun getAll(): List<T>
 }
